f13693278159156cc0279fc60968456b254d3b3f
kaadon.com <kaadon.com@gmail.com>
2023-10-24 13:53:33 +0800

    commit

--- src/config/getEnvConfig.js
+++
@@ -1,8 +0,0 @@
-export function getEnvConfig(){
-    const Options = process.env?.TELEGRAM
-    if (!Options) throw new Error("TELEGRAM 配置不存在");
-    const telegramOptions = JSON.parse(Options)
-    return {
-        token:telegramOptions?.TOKEN,
-    }
-}
